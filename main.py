020477f67205d3be2645c3f8077c25aee48a0959
MiniGirlGeek <minigirlgeek@gmail.com>
2015-07-14 11:45:20 +0100

    Make fruit appear to fall inside bin

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ apple = Actor('apple-good')
 badApple = Actor('apple-rotten')
 scoreBase = Actor('score-base')
 potTop = Actor('pot-top')
+binTop = Actor('bin-top')
+binInside = Actor('bin-inside')
 
 clock_img.pos = 110, 110
 pot.pos = 500, 500
@@ -14,12 +16,14 @@ bin.pos = 98.5, 490.398
 apple.pos = 500, 0
 badApple.pos = 508.889, 0
 potTop.pos = 500, 511.5
+binInside.pos = 98.5, 440.705
+binTop.pos = 143.487, 453.034
 
 WIDTH = 800
 HEIGHT = 600
-MAX_SPEED = 30
+MAX_SPEED = 10
 
-speed = 2
+speed = 3
 
 appleType = apple
 
@@ -30,6 +34,8 @@ def open_bin():
 def close_bin():
     bin.image = 'closed-bin'
     bin.pos = 98.5, 490.398
+    apple.pos = 500, 0
+    badApple.pos = 508.889, 0
 
 
 def choose_apple():
@@ -44,15 +50,18 @@ def draw():
     screen.fill((237, 174, 0))
     clock_img.draw()
     pot.draw()
-    bin.draw()
+    #bin.draw()
+    binInside.draw()
     appleType.draw()
     potTop.draw()
+    binTop.draw()
 
 def on_mouse_down(pos):
-    open_bin()
-    clock.schedule_unique(close_bin, 1.0)
     if appleType.collidepoint(pos):
-        animate(appleType, pos=(103.059 , 326.068))
+        open_bin()
+        def drop_in_bin():
+            animate(appleType, duration=0.5,on_finished=close_bin, pos=(97.059, 575))
+        animate(appleType, duration=0.5,on_finished=drop_in_bin, pos=(97.059, 326.068))
     else:
         print("You missed me!")
 
@@ -64,7 +73,5 @@ def update():
         appleType.bottom = -100
         appleType = choose_apple()
         if speed < MAX_SPEED:
-            speed = speed * 1.2
+            speed = speed * 1.08
         print(speed)
-
-
